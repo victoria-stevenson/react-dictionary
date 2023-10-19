3de7abe06086e0ada87f48bdb40154ae85fed73c
VSWild <134617590+victoria-stevenson@users.noreply.github.com>
2023-10-19 12:19:44 +0200

    added API, located word definition path

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [word, setWord] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0].definition);
+  }
+
   function searchWord(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
+    let apiKey = "cd1da53fbt4d9c2776oe462faaba011d";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateWord(event) {
